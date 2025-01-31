4bd0ce69044345b4421add09b5a09d10f5a6e793
rohit kedar <rohitkedar2000@gmail.com>
2025-01-31 11:37:45 +0530

    completed auth routes for authentication

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express"
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+
 import authRoutes from "./routes/auth.route.js";
 dotenv.config()
 
@@ -9,7 +11,8 @@ const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
 
 const app = express();
-
+app.use(express.json())
+app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 
 app.listen(PORT,()=>{
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express"
 
-import { signup,login,logout} from  "../controllers/auth.controller.js"
+import { signup,login,logout,updateProfile, checkAuth} from  "../controllers/auth.controller.js"
+import { protectRoute } from "../middleware/auth.middleware.js"
 const router = express.Router()
 
 
@@ -8,5 +9,8 @@ router.post("/signup",signup)
 router.post("/login",login)
 router.post("/logout",logout)
 
+router.post("/update-profile",protectRoute,updateProfile)
+
+router.get("/check",protectRoute,checkAuth)
 
 export default router;
